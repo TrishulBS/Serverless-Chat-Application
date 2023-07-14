190e205f51a54c77627a9282c7c84763e327c1b3
TrishulBS <trishulcurtis@gmail.com>
2023-07-13 22:44:46 -0700

    Change in text window implemetation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState, useRef} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Welcome from "./Welcome"
 import WebSocketConnector from './WebSocketConnector';
@@ -8,23 +7,45 @@ import Sidebar from './Sidebar';
 
 const connector = new WebSocketConnector()
 
-type Client = {
+export type Client = {
    connectionId: string,
    nickname: string
+ }
+
+ export type Message = {
+   messageId: string
+   createdAt: number
+   nicknameToNickname: string
+   message: string
+   sender: string
  }
 
 function App() {
   const [nickname, setNickname] = useState(window.localStorage.getItem("nickname")|| "");
 
+  const [clients, setClients] = useState<Client[]>([]);
+  const [targetNicknameValue, setTargetNicknameValue] = useState(
+   window.localStorage.getItem("lastTargetNickname")|| ""
+  )
+  const [messages, setMessages] = useState<Message[]>([])
+
   useEffect(() => {
    window.localStorage.setItem("nickname", nickname)
+   window.localStorage.setItem("lastTargetNickname", targetNicknameValue)
+
   })
 
   const webSocket = useRef(connector)
 
 
   if (nickname===""){
-    return <Welcome setNickname={setNickname}/>
+    return <Welcome 
+    setNickname={(nickname) =>{
+      setNickname(nickname)
+      if(targetNicknameValue === ""){
+         setTargetNicknameValue(nickname)
+      }
+    }}/>
   }
 
 
@@ -36,25 +57,83 @@ function App() {
    ws.send(JSON.stringify({
       action: "getClients"
    }))
+
+   loadMessages(targetNicknameValue)
+  }
+
+  const loadMessages = (nickname: string) => {
+   ws.send(JSON.stringify({
+      action: "getMessages",
+      targetNickname: nickname,
+      limit: 1000,
+   }))
   }
 
   ws.onmessage = (e) => {
+   console.log(e)
    const message = JSON.parse(e.data) as {
       type: string
-      value: {
-         clients: Client []
-      }
+      value: unknown
    }
-   console.log(message.value)
+
+   console.log(message)
+   if (message.type === 'clients'){
+      setClients((message.value as {clients: Client[]}).clients)
+   }
+   if (message.type === 'messages'){
+      setMessages((message.value as {messages: Message[]}).messages.reverse())
+   }
+
+   if (message.type === 'message'){
+      const messageValue = message.value as {message: Message}
+
+      if (messageValue.message.sender === targetNicknameValue){
+         setMessages([...messages, messageValue.message])
+      }  
+   } 
   }
+
+  const setTargetNickname = (nickname: string) => {
+   setMessages([])
+   setTargetNicknameValue(nickname)
+   loadMessages(nickname)
+   setTargetNicknameValue(nickname)
+  }
+
+  const sendMessage = (message: string) => {
+      ws.send(JSON.stringify({
+         action: "sendMessage",
+         message,
+         recipientNickname: targetNicknameValue
+      }))
+
+      setMessages([
+         ...messages,
+         {
+            message: message,
+            sender: nickname,
+            createdAt: new Date().getTime(),
+            messageId: Math.random().toString(),
+            nicknameToNickname: [nickname, targetNicknameValue].sort().join("#")
+         }
+        ])
+  }
+
+  
 
   return (
    <div className="flex">
-      <div className = "flex-none w-16 md:w-40 border-r-2">
-         <Sidebar></Sidebar> 
+      <div className="flex-none w-16 md:w-40 border-r-2">
+         <Sidebar 
+         nickname = {nickname}
+         clients = {clients} setTargetNickname={setTargetNickname}></Sidebar>
       </div>
-      <div className='flex-auto'>
-         <Conversation></Conversation>
+      <div className="flex-auto">
+       <Conversation 
+         nickname = {nickname}
+         targetNickname={targetNicknameValue}
+         messages={messages} 
+         sendMessage={sendMessage}></Conversation>
       </div>
       
       
--- frontend/src/Sidebar.tsx
+++ frontend/src/Sidebar.tsx
@@ -1,61 +1,38 @@
-import React from "react";
+import React from "react"
+import {Client} from './App'
 
-export default function Sidebar({
-  me,
-  clients,
-  setTarget,
+const Sidebar = ({
+    nickname,
+    clients,
+    setTargetNickname
 }: {
-  me: string;
-  clients: string[];
-  setTarget: (target: string) => void;
-}) {
-  return (
-    <>
-      <div className="flex-none border-r-2 border-gray-200 w-20 md:w-64">
-        <div className="flex sm:items-center justify-between py-10 px-5">
-          <div className="flex items-center space-x-2">
-            <img
-              src={`doggos/${me}.jpeg`}
-              alt=""
-              className="w-8 rounded-full"
-            />
-            <div className="text-2xl invisible md:visible">
-              <span className="text-gray-700 mr-3 font-bold">Chats</span>
+    nickname: string
+    clients: Client[];
+    setTargetNickname: (nickname: string) => void
+}) => {
+    return (
+    <div className="flex flex-col md:ml-2">
+        <div className="flex justify-center mt-8 mb-7 md: justify-start">
+            <img src={`/public/doggos/${nickname}.jpeg`}
+            alt="" 
+            className="w-7  rounded-full mr-2 "/>
+        <span className="invisible md:visible w-0 md:w-auto font-medium">Chats</span>
+        </div>
+        <div className="flex flex-col items-center md:items-start">
+            {clients.map((client) => (
+            <button onClick={() => setTargetNickname(client.nickname)}>
+                <div className="flex mb-3">
+                <img src={`doggos/${client.nickname}.jpeg`}
+                alt="" 
+                className="w-8 h-8 rounded-full mr-1.5"/>
+            <span className="text-sm leading-8 invisible md:visible w-0 md:w-auto ">{client.nickname}</span>
             </div>
-          </div>
-        </div>
-        <div className="flex flex-col px-2">
-          <div className="flex flex-col">
-            {clients.map((client, key) => (
-              <div key={key}>
-                <button
-                  className="flex items-center p-3 bg-white rounded-xl"
-                  onClick={() => setTarget(client)}
-                >
-                  <img
-                    src={`doggos/${client}.jpeg`}
-                    alt=""
-                    className="w-4 sm:w-8 h-10 sm:h-8 rounded-full"
-                  />
-                  <div className="flex items-center">
-                    <span className="mr-3 ml-2">{client}</span>
-                    <span className="text-green-500">
-                      <svg width="10" height="10">
-                        <circle
-                          cx="5"
-                          cy="5"
-                          r="5"
-                          fill="currentColor"
-                        ></circle>
-                      </svg>
-                    </span>
-                  </div>
-                </button>
-              </div>
+            </button>
+            
             ))}
-          </div>
         </div>
-      </div>
-    </>
-  );
+    </div>
+    )
 }
+
+export default Sidebar
